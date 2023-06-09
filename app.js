5fc27402a9e81c103cb325f0e35791158d858d0e
Emanuel Arroyo <ema201217@gmail.com>
2023-06-09 20:55:59 -0300

    feat: wip in server path

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const express = require("express");
+const path = require('path')
 const PORT = 3030;
 const app = express();
 
-
+// req --> request  --> lo que recibo del cliente
+// res --> response  --> lo que enviamos nosotros al cliente
+app.get('/home',(req,res) => {
+  const pathFileHome = path.join(__dirname,"./views/home.html")
+  res.sendFile(pathFileHome)
+})
 
 app.listen(PORT, () => {
   console.log(`http://localhost:${PORT}`);
