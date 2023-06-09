c7d573ed1d3367338fad255b73f2cf9e776e3b26
unknown <solvargas094@gmail.com>
2023-06-09 20:49:51 -0200

    Add data

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 const express = require("express");
 const PORT = 3030;
+const path = require('path')
 const app = express();
 
+ app.get('/gise',(req,res) => {
+res.sendFile(path.join(__dirname,'./views/gisela.html'))
+ })
+
+ app.get('/mati',(req,res) => {
+    res.sendFile(path.join(__dirname,'./views/matias.html'))
+ })
 
 
 app.listen(PORT, () => {
   console.log(`http://localhost:${PORT}`);
 });
+                                      
