b16bfba1d831a7e5f86e5dc88c29f289d9d6a690
Emanuel Arroyo <ema201217@gmail.com>
2023-06-16 22:17:08 -0300

    clase07 anatomia web

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const PORT = 3000;
+const path = require("path");
+
+app.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "./views/home.html"))
+);
+
+
+app.listen(PORT, console.log(`http://localhost:${PORT}`));
