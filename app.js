a7d8d079537271a24f4b67d9c521773843218b97
Emanuel Arroyo <ema201217@gmail.com>
2023-06-23 21:03:06 -0300

    imports css + algunos estilos

--- app.js
+++ app.js
@@ -3,9 +3,14 @@ const app = express();
 const PORT = 3000;
 const path = require("path");
 
+app.use(express.static("public"));
+
 app.get("/", (req, res) =>
   res.sendFile(path.join(__dirname, "./views/home.html"))
 );
 
+app.get("/detalle-producto", (req, res) =>
+  res.sendFile(path.join(__dirname, "./views/detalle-producto.html"))
+);
 
 app.listen(PORT, console.log(`http://localhost:${PORT}`));
