f96cedb3fdefb136fc389a2ffd71c8c6e6de2645
Emanuel Arroyo <ema201217@gmail.com>
2023-07-07 22:32:46 -0300

    flexbox y dando estilos a imagenes

---
+++ src/app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+const PORT = 3030
+
+app.use(express.static('public'))
+
+
+app.get('/',(req,res) => {
+  res.sendFile(path.join(__dirname,'./views/home.html'))
+})
+
+app.listen(PORT,() => console.log(`http://localhost:${PORT}`))
